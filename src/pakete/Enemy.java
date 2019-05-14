bbf6e5155cf5bad2bafd139863feb5e38f0a715d
Lukas.Kapust <dasidb>
2019-05-14 23:18:58 +0200

    ...

---
+++ src/pakete/Enemy.java
@@ -0,0 +1,15 @@
+package pakete;
+
+import processing.core.PImage;
+
+import java.util.ArrayList;
+
+public class Enemy {
+    private int life;
+    private PImage image;
+    private int damage;
+    private float positionX;
+    private float positionY;
+    private ArrayList<Enemy> enemyList;
+
+}
